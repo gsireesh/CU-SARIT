c2f80a6588bac67676746794f61a55276bc0ad71
Sireesh Gururaja <skg2123@columbia.edu>
2014-03-09 14:19:22 -0400

    regexes working on text

--- Tantravārttika/TVSplitter.py
+++ Tantravārttika/TVSplitter.py
@@ -1,18 +1,36 @@
 import re
 import os
 
-d2r_num = {'०':0, '१':1, '२':2, '३':3, '४':4, '५':5, '६':6,'७':7,'८':8,'९':9}
+FILENAME = 'volume2/TV2.xml'
+base_adhyaya = 2
+base_pada = 2
 
-adhi_re = re.compile('इति(\D*धिकरणम्).*(\d)', 0)
+d2r_num = {'०':'0', '१':'1', '२':'2', '३':'3', '४':'4', '५':'5', '६':'6','७':'7','८':'8','९':'9'}
 
-match = adhi_re.match('इति शब्दान्तराधिकरणम् ॥ १ ॥')
-if(match):
-    print(match.group(1))
-    print(d2r_num[match.group(2)])
-    print('general pattern works!')
-    
-if re.match('\d', '२'):
-    print('number pattern works!')
 
-print('''Regex to match adhikaranas with paadas: \
-.*?इति(—|--)(\D*धिकरणम्).*?(\d+).*\n</lg>\n(.*इति\D*पादः.*\n)?''')
+def num_d2r(number):
+	rString = '' 
+	for s in str(number):
+		rString += d2r_num[s]
+	return int(rString)
+
+
+
+adhi_re = re.compile('.*?इति(—|--| *)?(?P<name>\D*धिकरणम्).*?(?P<number>\d+).*')
+pada_re = re.compile('(.*इति\D*पादः.*\n)')
+
+
+f = open(FILENAME,'r')
+tvLines = f.readlines()
+tempLineArray = []
+outFile = open('log.txt', 'w')
+
+for i in range(len(tvLines)):
+	match = adhi_re.match(tvLines[i])
+	if(match):
+		print('Adhikarana:', match.group('name'), file=outFile)
+		print('Number:', num_d2r(match.group('number')), '\n', file=outFile)
+
+
+
+'''.*?इति(—|--)(\D*धिकरणम्).*?(\d+).*\n</lg>\n(.*इति\D*पादः.*\n)?'''
