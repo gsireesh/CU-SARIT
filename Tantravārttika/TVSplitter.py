61f94e9cd7ceb86372cf375df5d0879f7f64a3f6
Sireesh Gururaja <skg2123@columbia.edu>
2014-03-18 14:50:26 -0400

    major bugs solved, numbering working until 3.5.20

--- Tantravārttika/TVSplitter.py
+++ Tantravārttika/TVSplitter.py
@@ -2,8 +2,8 @@ import re
 import os
 
 FILENAME = 'volume2/TV2.xml'
-base_adhyaya = 2
-base_pada = 2
+adhyaya = 2
+pada = 2
 
 d2r_num = {'०':'0', '१':'1', '२':'2', '३':'3', '४':'4', '५':'5', '६':'6','७':'7','८':'8','९':'9'}
 
@@ -17,20 +17,37 @@ def num_d2r(number):
 
 
 adhi_re = re.compile('.*?इति(—|--| *)?(?P<name>\D*धिकरणम्).*?(?P<number>\d+).*')
-pada_re = re.compile('(.*इति\D*पादः.*\n)')
+pada_re = re.compile('(.*इति श्रीभट्टकुमारिलविरचिते मीमांसाभाष्यव्याख्याने.*\n)')
+adhyaya_re = re.compile(' <p>समाप्तश्च द्वितीयोऽध्यायः ॥</p>')
 
 
 f = open(FILENAME,'r')
 tvLines = f.readlines()
 tempLineArray = []
 outFile = open('log.txt', 'w')
+minLimit = 0
+maxLimit = 0
 
 for i in range(len(tvLines)):
 	match = adhi_re.match(tvLines[i])
 	if(match):
-		print('Adhikarana:', match.group('name'), file=outFile)
-		print('Number:', num_d2r(match.group('number')), '\n', file=outFile)
-
+		clean_name = re.sub('(<.*?>)|(/?/)', '', match.group('name'))
+		print(clean_name)
+		print('Adhikarana:', clean_name, file=outFile)
+		print(pada, '.', num_d2r(match.group('number')), '\n', file=outFile)
+		maxLimit = i + 2
+		if(i<len(tvLines)-2 and pada_re.match(tvLines[i+2])):
+			pada += 1
+			maxLimit = i+4
+		if(i<len(tvLines)-4 and adhyaya_re.match(tvLines[i+4])):
+			adhyaya += 1
+			pada = 1
+			maxLimit = i+4
+		div = str(adhyaya)+'.'+str(pada)+'.'+str(num_d2r(match.group('number')))
+		with open('TVTest/'+div+clean_name+'.xml', 'a+') as f:
+			for line in tvLines[minLimit:maxLimit]:
+				f.write(line)
+		minLimit = maxLimit
 
 
 '''.*?इति(—|--)(\D*धिकरणम्).*?(\d+).*\n</lg>\n(.*इति\D*पादः.*\n)?'''
